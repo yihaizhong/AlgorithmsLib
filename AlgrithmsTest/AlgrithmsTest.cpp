eeaaa7175131c41405d311eb9ba0946b4ba01cee
yihaizhong <yutaosea@gmail.com>
2016-12-22 18:13:11 +0800

    add merge-sort

--- AlgrithmsTest/AlgrithmsTest.cpp
+++ AlgrithmsTest/AlgrithmsTest.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include "InsertSort.h"
+#include "MergeSort.h"
 
 using namespace std;
 using namespace algorithms;
@@ -18,11 +19,19 @@ void show(const T *data, int n)
 }
 void TestInsertSort();
 
+void TestMerge();
+void TestMergeSort();
+
+
+
+
 
 
 int main()
 {
 	TestInsertSort();
+	TestMerge();
+	TestMergeSort();
     return 0;
 }
 
@@ -31,6 +40,27 @@ void TestInsertSort()
 {
 	typedef double ValueType;
 	InsertSort<ValueType> is;
+	ValueType a[] = { 15,20,25,3,2,5,3,2,1,4,53,5,3,4 };
+	cout << "before:" << endl;
+	show(a, sizeof(a) / sizeof(ValueType));
+	is.sort(a, sizeof(a) / sizeof(ValueType), ValueCompare);
+	cout << "after:" << endl;
+	show(a, sizeof(a) / sizeof(ValueType));
+}
+
+void TestMerge()
+{
+	double a[]{ 1,3,5,8,9,55, 0,4,6,8,40,58,59,60};
+	//double b[]{  };
+	MergeSort<double> ms;
+	ms.merge(a, 0, 5, 13,ValueCompare);
+	show(a, 14);
+}
+
+void TestMergeSort()
+{
+	typedef double ValueType;
+	MergeSort<ValueType> is;
 	ValueType a[] = { 15,20,25,3,2,5,3,2,1,4,53,5,3,4 };
 	cout << "before:" << endl;
 	show(a, sizeof(a) / sizeof(ValueType));
